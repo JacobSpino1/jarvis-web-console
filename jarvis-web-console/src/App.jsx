5943d70000069d784bc526481d6b9c90ab0db62b
JacobSpino1 <jacobspino@uccenvironmental.com>
2025-05-19 13:22:47 -0500

    Add files via upload

---
+++ jarvis-web-console/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-gray-950 text-white p-6'>
+      <h1 className='text-4xl font-bold mb-4'>JARVIS Web Console</h1>
+      <p className='text-lg mb-6'>Welcome back, Jacob. Systems are nominal.</p>
+      {/* Components go here */}
+    </div>
+  );
+}
